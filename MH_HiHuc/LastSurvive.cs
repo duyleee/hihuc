bcf84965fbfbcd0c4ef0dd44108fe7b202b7874b
Duy Le <lethanh.duy@live.com>
2017-08-16 07:06:46 +0700

    add sample antigravity

---
+++ MH_HiHuc/LastSurvive.cs
@@ -0,0 +1,124 @@
+﻿using MH_HiHuc.MH;
+using Robocode;
+using Robocode.Util;
+using System;
+using System.Drawing;
+
+namespace MH_HiHuc
+{
+    //https://www.ibm.com/developerworks/library/j-antigrav/index.html
+    public class LastSurvive : AdvancedRobot
+    {
+        static PointF[] enemyPoints = new PointF[5];
+        int count;
+        public override void Run()
+        {
+            while (true)
+            {
+                TurnRadarRight(360);
+            }
+        }
+        public override void OnScannedRobot(ScannedRobotEvent e)
+        {
+
+        }
+
+        void antiGravMove()
+        {
+            double xforce = 0;
+            double yforce = 0;
+            double force;
+            double ang;
+            GravPoint p;
+
+            for (int i = 0; i < gravpoints.size(); i++)
+            {
+                p = (GravPoint)gravpoints.elementAt(i);
+                //Calculate the total force from this point on us
+                force = p.power / Math.pow(getRange(getX(), getY(), p.x, p.y), 2);
+                //Find the bearing from the point to us
+                ang =
+            normaliseBearing(Math.PI / 2 - Math.atan2(getY() - p.y, getX() - p.x));
+                //Add the components of this force to the total force in their 
+                //respective directions
+                xforce += Math.sin(ang) * force;
+                yforce += Math.cos(ang) * force;
+            }
+
+            /**The following four lines add wall avoidance.  They will only 
+            affect us if the bot is close to the walls due to the
+            force from the walls decreasing at a power 3.**/
+            xforce += 5000 / Math.pow(getRange(getX(),
+              getY(), getBattleFieldWidth(), getY()), 3);
+            xforce -= 5000 / Math.pow(getRange(getX(),
+              getY(), 0, getY()), 3);
+            yforce += 5000 / Math.pow(getRange(getX(),
+              getY(), getX(), getBattleFieldHeight()), 3);
+            yforce -= 5000 / Math.pow(getRange(getX(),
+              getY(), getX(), 0), 3);
+
+            //Move in the direction of our resolved force.
+            goTo(getX() - xforce, getY() - yforce);
+        }
+
+        /**Move in the direction of an x and y coordinate**/
+        void goTo(double x, double y)
+        {
+            double dist = 20;
+            double angle = RadianToDegree(absoluteBearing(X, Y, x, y));
+            double r = turnTo(angle);
+            SetAhead(dist * r);
+        }
+
+        private double RadianToDegree(double angle)
+        {
+            return angle * (180.0 / Math.PI);
+        }
+        public double absoluteBearing(double x, double y, double x1, double y1)
+        {
+            return Math.Atan2(y1 - y, x1 - x);
+        }
+
+        /**Turns the shortest angle possible to come to a heading, then returns 
+the direction the bot needs to move in.**/
+        int turnTo(double angle)
+        {
+            double ang;
+            int dir;
+            ang = normalizeBearing(Heading - angle);
+            if (ang > 90)
+            {
+                ang -= 180;
+                dir = -1;
+            }
+            else if (ang < -90)
+            {
+                ang += 180;
+                dir = -1;
+            }
+            else {
+                dir = 1;
+            }
+            SetTurnLeft(ang);
+            return dir;
+        }
+
+
+        // normalizes a bearing to between +180 and -180
+        double normalizeBearing(double angle)
+        {
+            while (angle > 180) angle -= 360;
+            while (angle < -180) angle += 360;
+            return angle;
+        }
+
+        /*Returns the distance between two points**/
+        double getRange(double x1, double y1, double x2, double y2)
+        {
+            double x = x2 - x1;
+            double y = y2 - y1;
+            double range = Math.Sqrt(x * x + y * y);
+            return range;
+        }
+    }
+}
--- MH_HiHuc/TL.cs
+++ MH_HiHuc/TL.cs
@@ -1,25 +1,42 @@
 ﻿using Robocode;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MH_HiHuc
 {
     public class TL : TeamRobot
     {
-        private int wallMargin = 60;
+        
         double _randomDistance = 120;
+        private Random randomizer = new Random();
         public override void Run()
         {
-            var x = 0;var  y =0;
+            
+            var x = 0; var y = 0;
             IsAdjustGunForRobotTurn = true;
             while (true)
             {
                 TurnRadarRight(360);
             }
         }
+
+        private int wallMargin = 200;
+        private int tooCloseToWall = 0;
+        private void InitializeEvents()
+        {
+            AddCustomEvent("wall_reached", 1, new ConditionTest((condition) =>
+            {
+                return
+                // we're too close to the left wall
+                (X <= wallMargin ||
+                // or we're too close to the right wall
+                X >= BattleFieldWidth - wallMargin ||
+                // or we're too close to the bottom wall
+                Y <= wallMargin ||
+                // or we're too close to the top wall
+                Y >= BattleFieldHeight - wallMargin);
+            }));
+        }
+
         int moveDirection = 1;
         public override void OnScannedRobot(ScannedRobotEvent e)
         {
@@ -46,15 +63,25 @@ namespace MH_HiHuc
         }
         private void DoMoving(ScannedRobotEvent e)
         {
-            // always square off against our enemy
-            SetTurnRight(e.Bearing + 90);
-            SetTurnRight(normalizeBearing(e.Bearing + 90 - (15 * moveDirection)));
+            
 
-            // strafe by changing direction every 20 ticks
+            int IsTooCloseWithEnemy = e.Distance > 70 ? 1 : -1;
+            //SetTurnRight(normalizeBearing(e.Bearing + 90  - (15 * moveDirection * IsTooCloseWithEnemy)));
+            SetTurnRight(normalizeBearing(e.Bearing + 90-(15 * moveDirection)));
+
+            if (tooCloseToWall > 0) tooCloseToWall--;
+
+            // normal movement: switch directions if we've stopped
+            if (Velocity == 0)
+            {
+                moveDirection *= -1;
+                SetAhead(100 * moveDirection);
+            }
+            else
+
             if (DistanceRemaining == 0)
             {
                 moveDirection *= -1;
-                _randomDistance = (new Random().Next(100, 200));
                 SetAhead(_randomDistance * moveDirection);
             }
         }
@@ -69,21 +96,20 @@ namespace MH_HiHuc
 
         public override void OnHitByBullet(HitByBulletEvent evnt)
         {
-            
+            //_randomDistance = (randomizer.Next(100, 200));
         }
 
-        private void goTo(int x, int y)
+        public override void OnCustomEvent(CustomEvent evnt)
         {
-            double a;
-            SetTurnRightRadians(Math.Tan(
-                a = Math.Atan2(x -= (int)X, y -= (int)Y)
-                      - HeadingRadians));
-            SetAhead(Hypot(x, y) * Math.Cos(a));
-        }
-
-        private double Hypot(int x, int y)
-        {
-            return System.Math.Sqrt(x * x + y * y);
+            if (evnt.Condition.name == "wall_reached")
+            {
+                if (tooCloseToWall <= 0)
+                {
+                    // if we weren't already dealing with the walls, we are now
+                    tooCloseToWall += wallMargin;
+                    MaxVelocity = 0; // stop!!!
+                }
+            }
         }
     }
 }
