083f204687aae5d31d611d11bb5b6b7b35627a8f
Duy Le <lethanh.duy@live.com>
2017-08-20 02:57:36 +0700

    simpler solo movement

--- MH_HiHuc/Strategies/Solo.cs
+++ MH_HiHuc/Strategies/Solo.cs
@@ -23,79 +23,74 @@ namespace MH_HiHuc.Strategies
             MyBot.IsAdjustGunForRobotTurn = true;
         }
 
-        public void OnHitByBullet(HitByBulletEvent e)
-        {
-            _randomDistance += new Random().NextDouble() * 10 * _turn;
-
-            if (_randomDistance <= 50)
-            {
-                _randomDistance = 50;
-            }
-
-            if (_randomDistance >= 400)
-            {
-                _randomDistance = 400;
-            }
-        }
-
-        public void OnHitRobot(HitRobotEvent evnt)
-        {
-            MyBot.Fire(3);
-            _randomDistance += new Random().NextDouble() * 10 * _turn;
-
-            if (_randomDistance <= 50)
-            {
-                _randomDistance = 50;
-            }
-
-            if (_randomDistance >= 400)
-            {
-                _randomDistance = 400;
-            }
-        }
-
-        public void OnPaint(IGraphics graphics)
-        {
-            
-        }
-
         public void OnScannedRobot(ScannedRobotEvent e)
         {
             if (MyBot.IsTeammate(e.Name))
             {
                 return;
             }
-            //----------------------------------------------------MOVEMENT-------------------------------------------
+            Move(e);
+            RadarAdjust(e);
+            Fire(e);
+        }
 
-            if (MyBot.DistanceRemaining == 0) // make robot move every turn
+        private int turnDirection = 1;
+        private int moveDirection = 1;
+        private int randomTurnFactor = 5;
+        private double randomDistance = 200.0;
+        private void Move(ScannedRobotEvent e)
+        {
+            if (MyBot.DistanceRemaining == 0.0)
             {
-                //reverse move direction, if previous time move ahead => this time move back and vice versa
-                _move = _move * -1;
-                //reverse turn direction, if previous time turn right => this time turn left and vice versa
-                _turn = _turn * -1;
-                //move forward or backward
-                MyBot.SetAhead(_randomDistance * _move);
+                moveDirection *= -1; //moving backward
+                turnDirection *= -1; //moving in opposite direction
+                MyBot.SetAhead(randomDistance * moveDirection);
             }
 
-            //e.BearingRadians + Math.PI / 2 => turn tank body always block before enemy
-            //then random +- Math.PI/5 -> Math.PI/7 => make random movement, to dodge Linear and circular targeting from enemy                        
-            var angle = Math.PI / _number;
-            _number++;
-            if (_number >= 7)
+            int distantCoefficient = (e.Distance > 150.0) ? 1 : -1;
+            double coefficientAngel = Math.PI / randomTurnFactor;
+            randomTurnFactor++;
+            if (randomTurnFactor >= 7)
             {
-                _number = 5;
+                randomTurnFactor = 5;
             }
-            int IsTooCloseWithEnemy = e.Distance > 50 ? 1 : -1;
-            MyBot.SetTurnRightRadians((e.BearingRadians + Math.PI / 2) - (angle * _move) * (IsTooCloseWithEnemy));
+            MyBot.SetTurnRightRadians(e.BearingRadians + Math.PI / 2 - coefficientAngel * moveDirection * distantCoefficient);
+        }
 
-            //----------------------------------------------------RADAR-------------------------------------------
+        public void OnHitByBullet(HitByBulletEvent e)
+        {
+            randomDistance += new Random().NextDouble() * 50.0 * turnDirection;
+            if (randomDistance <= 100.0)
+            {
+                randomDistance = 100.0;
+            }
+            if (randomDistance >= 400.0)
+            {
+                randomDistance = 400.0;
+            }
+        }
+        public void OnHitRobot(HitRobotEvent evnt)
+        {
+            randomDistance += new Random().NextDouble() * 50.0 * turnDirection;
+            if (randomDistance <= 100.0)
+            {
+                randomDistance = 100.0;
+            }
+            if (randomDistance >= 400.0)
+            {
+                randomDistance = 400.0;
+            }
+        }
 
+        private void RadarAdjust(ScannedRobotEvent e)
+        {
             //radar scan 1 vs 1 algorithm - perfect radar lock - http://robowiki.net/wiki/One_on_One_Radar
             double radarAngleToTurn = MyBot.HeadingRadians - MyBot.RadarHeadingRadians + e.BearingRadians;
             MyBot.SetTurnRadarRightRadians(Utils.NormalRelativeAngle(radarAngleToTurn));
+        }
 
-            //----------------------------------------------------GUN-------------------------------------------
-            //choose bullet power base on distance, if distance to enemy larger than 400 => bullet power = 1
+        private void Fire(ScannedRobotEvent e)
+        {
             var bullerPower = 400 / e.Distance;
 
             //simplest linear targeting algorithm - http://robowiki.net/wiki/Linear_Targeting
@@ -118,11 +113,12 @@ namespace MH_HiHuc.Strategies
 
         public void OnDroidMessage(string enemyName)
         {
+
         }
 
-        int _turn = 1; //1 is left, -1 is right
-        int _move = 1; //1 is ahead, -1 is back
-        int _number = 5;
-        double _randomDistance = 120;
+        public void OnPaint(IGraphics graphics)
+        {
+
+        }
     }
 }
