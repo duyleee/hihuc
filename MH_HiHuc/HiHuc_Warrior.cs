b4d84e7d5e1918f1425190b885fbc4bc99ab6936
Duy Le <duy.le@medhealthgroup.com.au>
2017-08-21 17:39:00 +0700

    add warror

---
+++ MH_HiHuc/HiHuc_Warrior.cs
@@ -0,0 +1,20 @@
+﻿using MH_HiHuc.Strategies;
+using MH_HiHuc.Base;
+using Robocode;
+
+namespace MH_HiHuc
+{
+    public class HiHucWarrior : HiHucCore
+    {
+        public override void Run()
+        {
+            Stragegy = new Solo(this);
+            Stragegy.Init();
+
+            while (true)
+            {
+                Stragegy.Run();
+            }
+        }
+    }
+}
